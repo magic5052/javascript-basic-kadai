f7cf59e1707a7531c02976472d42cb6923a5db64
shu <magic5052@outlook.com>
2024-04-05 00:35:24 +0900

    kadai005 completed

---
+++ kadai_005/bmi.js
@@ -0,0 +1,8 @@
+const weight = 70;
+const height = 1.78;
+
+const bmi = weight / (height * height);
+
+console.log("体重：" + weight + " kg");
+console.log("身長：" + height + " m");
+console.log("bmi：" + bmi);
